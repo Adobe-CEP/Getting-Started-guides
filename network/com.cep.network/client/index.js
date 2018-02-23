451d5ade7c1959bb88886d686ae1ae2547d1fa2e
Ash <ashryan@adobe.com>
2018-02-23 17:18:13 -0500

    Showing the weather as text in the UI.

--- network/com.cep.network/client/index.js
+++ network/com.cep.network/client/index.js
@@ -1,12 +1,13 @@
 /*
   CSInterface
 */
-var csInterface = new CSInterface();
+const csInterface = new CSInterface();
 
 /*
   UI Elements
 */
-var greetingButton = document.querySelector("#greeting-button");
+const greetingButton = document.querySelector("#greeting-button");
+const weatherSummary = document.querySelector("#weather-summary");
 
 /*
   Event listeners
@@ -20,10 +21,30 @@ function alertGreeting() {
   csInterface.evalScript("sayHello()");
 }
 
-fetch(`https://api.darksky.net/forecast/${darkSkyKey}/37.8267,-122.4233?exclude=minutely,hourly,daily,alerts,flags`)
+const darkSkyExcludes = "minutely,hourly,daily,alerts,flags";
+
+const weatherTypes = {
+  "clear-day": "a clear day",
+  "clear-night": "a clear night",
+  "rain": "raining",
+  "snow": "snowing",
+  "sleet": "sleeting",
+  "wind": "windy",
+  "fog": "foggy",
+  "cloudy": "cloudy",
+  "partly-cloudy-day": "a partly cloudy day",
+  "partly-cloudy-night": "partly cloudy night"
+}
+
+let currentWeather;
+
+fetch(`https://api.darksky.net/forecast/${darkSkyKey}/37.8267,-122.4233?exclude=${darkSkyExcludes}`)
   .then(function(res) {
-    if (res.ok) return res.json()
+    if (res.ok) return res.json();
+
+    // handle res failure
   })
   .then(function(json) {
-    console.log(json);
-  })
+    currentWeather = weatherTypes[json.currently.icon];
+    weatherSummary.textContent = `It looks like it's ${currentWeather}.`;
+  });
